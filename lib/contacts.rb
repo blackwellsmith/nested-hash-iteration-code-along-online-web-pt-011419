a0a38a00cb6e2486322776e2b7c498f88102c406
blackwell <blackwellsmithiv@gmail.com>
2019-01-26 16:49:03 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,7 +1,6 @@
 require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
