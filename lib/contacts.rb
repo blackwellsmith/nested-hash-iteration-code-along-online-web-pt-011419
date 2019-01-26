404ac2c2d7af9f8513013f37181ee4013f57201d
blackwell <blackwellsmithiv@gmail.com>
2019-01-26 16:27:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,6 @@
 require 'pry'
-def remove_strawberry(x)
-  x.each do |person, contact_detail_hash|
+def remove_strawberry(contact)
+  contact.each do |person, contact_detail_hash|
     binding.pry
 end    
 
